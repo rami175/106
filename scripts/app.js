0d25b66c7c94a8917cc0f3dbc8c16c568e679b50
Rami <you@example.com>
2024-03-08 04:14:30 -0800

    final report

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,36 @@ function saveTask() {
 	const status = $("#inputStatus").val();
 	const color = $("#inputColor").val();
 
-	console.log(title, description, startData, budget, status, color);
+	let task = new Task(title, description, startData, budget, status, color);
+	console.log(task);
+
+	// save to server
+
+	$.ajax({
+		type: "POST",
+		url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+		data: JSON.stringify(task),
+		contentType: "application/json",
+		success: function (response) {
+			console.log(response);
+		},
+		error: function (error) {
+			console.log(error);
+		},
+	});
+}
+
+function testRequest() {
+	$.ajax({
+		type: "GET",
+		url: "http://fsdiapi.azurewebsites.net/",
+		success: function (response) {
+			console.log(response);
+		},
+		error: function (error) {
+			console.log(error);
+		},
+	});
 }
 
 function init() {
---
+++ scripts/task.js
@@ -0,0 +1,10 @@
+class Task {
+	constructor(title, description, startDate, budget, status, color) {
+		this.title = title;
+		this.description = description;
+		this.startDate = startDate;
+		this.budget = budget;
+		this.status = status;
+		this.color = color;
+	}
+}
